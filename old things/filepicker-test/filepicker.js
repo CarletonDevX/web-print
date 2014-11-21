eff61aaa94eea33e0a8b7239a816da2be803111d
Ken Schiller <kenschiller@gmail.com>
2014-11-21 16:47:31 -0600

    Remove old things from repo

--- old things/filepicker-test/filepicker.js
+++
@@ -1,6 +0,0 @@
-gotfile = function(event) {
-    var file = event.fpfile;
-    if (file) {
-        console.log("File uploaded to " + file.url);
-    }
-}
--- old things/joe/background.js
+++
@@ -1,6 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(){
-	//option A: Open a new tab
-	chrome.tabs.create({url: "main.html"})
-	//option B: Open a new window
-	//chrome.windows.create({url: "main.html", type: "popup"})
-});
