f3ed6938f0cbedfe5fe11ed0242633d52b50cadb
dpikt <pickartd@carleton.edu>
2015-01-27 14:26:39 -0600

    Increase timeout time (again)

--- chrome/scripts/app/main.js
+++ chrome/scripts/app/main.js
@@ -222,7 +222,7 @@ var attemptRelease = function (data, attempt, copies) {
   if (copies == 0) {
     data.success = 1;
     finishPrint(data);
-  } else if (attempt > 50) {
+  } else if (attempt > 200) {
     finishPrint(data);
   } else {
     printMessage("Releasing copy " + copies + "...");
