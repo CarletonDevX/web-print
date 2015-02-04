fa3954b4420f97ce18b65d1e83096765a96e1c15
dpikt <pickartd@carleton.edu>
2015-02-03 21:58:18 -0600

    More autorelease info

--- chrome/scripts/app/printers.js
+++ chrome/scripts/app/printers.js
@@ -11,8 +11,8 @@ define([], function () {
     { name: 'Cowling 108 (Color)', long_name: 'print\\COWL108-CC5051', page: 1 },
     { name: 'Facilities 318 (Color)', long_name: 'print\\FACI318-CC5051', page: 1 },
     { name: 'Facilities 335', long_name: 'print\\FACI335-LJ4250', page: 1 },
-    { name: 'Goodsell 102', long_name: 'print\\GOOD102-X4500', page: 1 },
-    { name: 'Goodsell 103', long_name: 'print\\GOOD103-X6350', page: 1 },
+    { name: 'Goodsell 102', long_name: 'print\\GOOD102-X4500', page: 1, autorelease: false },
+    { name: 'Goodsell 103', long_name: 'print\\GOOD103-X6350', page: 1, autorelease: false },
     { name: 'Goodsell 103 (Color)', long_name: 'print\\GOOD103-CC5051', page: 1 },
     { name: 'Goodhue 156', long_name: 'print\\GHUE156-X5550', longitude: -93.149809, latitude: 44.462625 , page: 1 },
     { name: 'HOPN 104 (Color)', long_name: 'print\\HOPN104-CC5051', page: 1 },
@@ -22,7 +22,7 @@ define([], function () {
     { name: 'Hulings 100 (Color)', long_name: 'print\\HUL100-CC5051', page: 1 },
     { name: 'Laird 115 (Color)', long_name: 'print\\LAIR115-CC5051', page: 1 },
     { name: 'Laird 118 (Color)', long_name: 'print\\LAIR118-CC5051', page: 1 },
-    { name: 'Laird 208', long_name: 'print\\LAIR208-X4600', page: 1 },
+    { name: 'Laird 208', long_name: 'print\\LAIR208-X4600', page: 1, autorelease: false },
     { name: 'Laird 208 (Color)', long_name: 'print\\LAIR208-CC5051', page: 1 },
     { name: 'Laird 300', long_name: 'print\\LAIR300-X4500', page: 1 },
     { name: 'LAST 110 (Color)', long_name: 'print\\LAST110-CC5051', page: 1 },
@@ -34,7 +34,7 @@ define([], function () {
     { name: 'Leighton 231', long_name: 'print\\LEIG231-X4600', page: 2 },
     { name: 'Leighton 326', long_name: 'print\\LEIG326-X4600', page: 2 },
     { name: 'Leighton 326 (Color)', long_name: 'print\\LEIG326-CC5051', page: 2 },
-    { name: 'Leighton 414', long_name: 'print\\LEIG414-LJM602', page: 2 },
+    { name: 'Leighton 414', long_name: 'print\\LEIG414-LJM602', page: 2, autorelease: false },
     { name: 'Library 400', long_name: 'print\\LIBR-Public-X5550', longitude: -93.154705, latitude: 44.462281, page: 2 },
     { name: 'Library 400 (Color)', long_name: 'print\\LIBR400-CC5051', page: 2 },
     { name: 'Mudd 075', long_name: 'print\\MUDD075-X4510', page: 2 },
@@ -46,8 +46,8 @@ define([], function () {
     { name: 'Olin 011', long_name: 'print\\OLIN011-X3600', page: 2 },
     { name: 'Olin 104', long_name: 'print\\OLIN104-X4510', page: 2 },
     { name: 'Olin 112', long_name: 'print\\OLIN112-X4510', page: 2 },
-    { name: 'Olin 125 (Color)', long_name: 'print\\OLIN125-CC5051', page: 2 },
-    { name: 'Olin 215 (Color)', long_name: 'print\\OLIN215-CC5051', page: 2 },
+    { name: 'Olin 125 (Color)', long_name: 'print\\OLIN125-CC5051', page: 2, autorelease: false },
+    { name: 'Olin 215 (Color)', long_name: 'print\\OLIN215-CC5051', page: 2, autorelease: false },
     { name: 'Olin 301', long_name: 'print\\OLIN301-X4600', longitude: -93.152821, latitude: 44.461334, page: 3 },
     { name: 'Olin 311', long_name: 'print\\OLIN311-X6350', page: 3 },
     { name: 'Olin 311 (Color)', long_name: 'print\\OLIN311-CC5051', page: 3 },
@@ -57,7 +57,7 @@ define([], function () {
     { name: 'Sayles 050', long_name: 'print\\SAYL050-X6360', page: 3 },
     { name: 'Sayles 057', long_name: 'print\\SAYL057-CC5051', page: 3 },
     { name: 'Sayles 109', long_name: 'print\\SAYL109A-X4510', page: 3 },
-    { name: 'Sayles 150', long_name: 'print\\SAYL150-X4510', page: 3 },
+    { name: 'Sayles 150', long_name: 'print\\SAYL150-X4510', page: 3, autorelease: false },
     { name: 'Scoville 014 (Color)', long_name: 'print\\SCOV014-CC5051', page: 3, autorelease: false },
     { name: 'Severance 014 (Color)', long_name: 'print\\SEVY014-CC5051', page: 3 },
     { name: 'Severance 129 (Color)', long_name: 'print\\SEVY129-CC5051', page: 3 },
@@ -71,7 +71,7 @@ define([], function () {
     { name: 'West Gym 200 (Color)', long_name: 'print\\WEST200-CC5051 ', page: 3 },
     { name: 'Willis 119', long_name: 'print\\WILL119-X4600', longitude: -93.156000, latitude: 44.460783, page: 3 },
     { name: 'Willis 310 (Color)', long_name: 'print\\WILL310-CC5051 ', page: 3 },
-    { name: 'Willis 409', long_name: 'print\\WILL409-X4600', page: 3 }
+    { name: 'Willis 409', long_name: 'print\\WILL409-X4600', page: 3, autorelease: false }
   ];
 
   return {
